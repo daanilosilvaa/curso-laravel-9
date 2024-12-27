34c1dc8dcdedccef06d3b89371cec3538e182112
daanilosilvaa <daanilo-silvaa@hotmail.com>
2024-12-27 12:49:36 -0300

    validate and request

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -6,10 +6,19 @@
 @section('content')
     <h1>Novo Usuario</h1>
 
+    @if ($errors->any())
+    <ul class="errors">
+        @foreach ($errors->all() as $error)
+            <li class="error">{{ $error }}</li>
+        @endforeach
+    </ul>
+
+    @endif
+
    <form action="{{ route('users.store') }}" method="post">
     @csrf
-    <input type="text" name="name" id="" placeholder="Nome:">
-    <input type="email" name="email" id="" placeholder="Email:">
+    <input type="text" name="name" id="" placeholder="Nome:" value="{{ old('name') }}">
+    <input type="email" name="email" id="" placeholder="Email:" value="{{ old('email') }}">
     <input type="password" name="password" id="" placeholder="Senha:">
     <button type="submit">Enviar</button>
    </form>
